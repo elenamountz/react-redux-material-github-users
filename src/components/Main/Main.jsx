737230ed84b9ec6d48b3118657baa18d2e48925f
Elena Mountzia <el.mountzia@gmail.com>
2021-04-02 00:30:01 +0300

    Initialize user container

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import AppBar from 'components/layout/AppBar';
-import SearchBar from 'components/SearchBar';
+import SearchBar from 'components/Main/SearchBar';
 import { Container, makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
